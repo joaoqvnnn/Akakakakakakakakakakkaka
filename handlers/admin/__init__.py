993bf8949cb37ee71dc9a39187dec9c772b92cd5
joaoqvnnn <empresadefofocas@gmail.com>
2026-09-02 19:52:54 -0300

    Add broadcast router to admin setup

--- handlers/admin/__init__.py
+++ handlers/admin/__init__.py
@@ -7,6 +7,7 @@ from handlers.admin.users import router as users_router
 from handlers.admin.messages import router as messages_router
 from handlers.admin.giftcards import router as giftcards_router
 from handlers.admin.payments import router as payments_router
+from handlers.admin.broadcast import router as broadcast_router
 
 
 def setup_admin_routers() -> Router:
@@ -18,4 +19,5 @@ def setup_admin_routers() -> Router:
     router.include_router(messages_router)
     router.include_router(giftcards_router)
     router.include_router(payments_router)
+    router.include_router(broadcast_router)
     return router
