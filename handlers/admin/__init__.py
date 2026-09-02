d27472f890b0237480824ae5f7238fb31be4943b
joaoqvnnn <empresadefofocas@gmail.com>
2026-09-02 17:19:22 -0300

    Add new routers for messages, giftcards, and payments

--- handlers/admin/__init__.py
+++ handlers/admin/__init__.py
@@ -4,6 +4,9 @@ from handlers.admin.panel import router as panel_router
 from handlers.admin.config import router as config_router
 from handlers.admin.logins import router as logins_router
 from handlers.admin.users import router as users_router
+from handlers.admin.messages import router as messages_router
+from handlers.admin.giftcards import router as giftcards_router
+from handlers.admin.payments import router as payments_router
 
 
 def setup_admin_routers() -> Router:
@@ -12,4 +15,7 @@ def setup_admin_routers() -> Router:
     router.include_router(config_router)
     router.include_router(logins_router)
     router.include_router(users_router)
+    router.include_router(messages_router)
+    router.include_router(giftcards_router)
+    router.include_router(payments_router)
     return router
