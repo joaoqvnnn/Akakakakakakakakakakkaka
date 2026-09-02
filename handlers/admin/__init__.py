965005a18cbcd73bab3529b13a401e49c3c02ef4
joaoqvnnn <empresadefofocas@gmail.com>
2026-09-02 17:09:12 -0300

    Add admin router setup in __init__.py

---
+++ handlers/admin/__init__.py
@@ -0,0 +1,15 @@
+from aiogram import Router
+
+from handlers.admin.panel import router as panel_router
+from handlers.admin.config import router as config_router
+from handlers.admin.logins import router as logins_router
+from handlers.admin.users import router as users_router
+
+
+def setup_admin_routers() -> Router:
+    router = Router()
+    router.include_router(panel_router)
+    router.include_router(config_router)
+    router.include_router(logins_router)
+    router.include_router(users_router)
+    return router
