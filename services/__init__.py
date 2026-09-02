7c55c7cb5c5c3ccb45dc0ee3f8faa1f82f33bfea
joaoqvnnn <empresadefofocas@gmail.com>
2026-09-02 13:21:53 -0300

    Create __init__.py to expose service classes
    Initialize services module and define public API.

---
+++ services/__init__.py
@@ -0,0 +1,19 @@
+from services.settings_service import SettingsService
+from services.balance import BalanceService
+from services.stock import StockService
+from services.purchase import PurchaseService
+from services.payment import PaymentService
+from services.giftcard import GiftCardService
+from services.affiliate import AffiliateService
+from services.alerts import AlertService
+
+__all__ = [
+    "SettingsService",
+    "BalanceService",
+    "StockService",
+    "PurchaseService",
+    "PaymentService",
+    "GiftCardService",
+    "AffiliateService",
+    "AlertService",
+]
